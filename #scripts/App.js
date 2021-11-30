9674897d54a2fbff66bb6384dc2b514225a6a06c
Josh Landen <24joshua.landen@boiseschools.net>
2021-11-30 11:37:59 -0700

    Cleaned up script

--- #scripts/App.js
+++ #scripts/App.js
@@ -1,5 +1,8 @@
 console.log(document.body);
-console.log("      /| ________________");
-console.log("O|===|* >________________>");
-console.log("      \|");
-/*sword*/
+console.log("Problem 1: 79 + 3 * (4 + 82 - 68) -7 + 19");
+console.log("Problem 2: (179 + 21 + 10)/7 + 181");
+console.log("Problem 3: 10389 * 56 * 11 + 2246");
+
+console.log(79 + 3 * (4 + 82 - 68) -7 + 19);
+console.log((179 + 21 + 10)/7 + 181);
+console.log(10389 * 56 * 11 + 2246);
